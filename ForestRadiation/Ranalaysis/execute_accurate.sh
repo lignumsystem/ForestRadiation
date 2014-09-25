f0d45304d1b5aa52a50136e3477099863d04525d
gopalkri <>
2014-09-25 15:39:57 +0000

    added execute_accurate.sh to the repo

---
+++ ForestRadiation/Ranalaysis/execute_accurate.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+den=(150)
+seed=(787237 536199  1676779  546327  235663 787231 536197 1576775 446327 135663)
+
+dLen=${#den[@]}
+sLen=${#seed[@]}
+
+for (( i=0; i<${dLen}; i++ ));
+  do
+    mkdir Resultapp/${den[$i]}_accurate
+  for (( j=0; j<${sLen}; j++ ));
+   do
+      ./lig-radiation -generateLocations ${den[$i]} -manyTrees manytrees.txt -resultfile Resultapp/${den[$i]}_accurate/accurate_${seed[$j]}_den${den[$i]}.dat -seed ${seed[$j]} -radMethod 2  -treeDist 0.3 -X 20 -Y 20 -Z 10 -appendMode
+  done
+done
+
