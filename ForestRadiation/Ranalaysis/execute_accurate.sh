b80c2cf5adaba8f5c8b741a6062d0c2228ebeb29
matrps <>
2014-10-14 10:19:35 +0000

    Risto's changes for making pruction runs for ythe radiation article. Some parts of the script need to be changed (e.g. number of trees) or commented out for different runs.

--- ForestRadiation/Ranalaysis/execute_accurate.sh
+++ ForestRadiation/Ranalaysis/execute_accurate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-den=(150)
+den=(23)
 seed=(787237 536199  1676779  546327  235663 787231 536197 1576775 446327 135663)
 
 dLen=${#den[@]}
@@ -8,9 +8,10 @@ sLen=${#seed[@]}
 for (( i=0; i<${dLen}; i++ ));
   do
     mkdir Resultapp/${den[$i]}_accurate
+    echo Directory: Resultapp/${den[$i]}_accurate
   for (( j=0; j<${sLen}; j++ ));
    do
-      ./lig-radiation -generateLocations ${den[$i]} -manyTrees manytrees.txt -resultfile Resultapp/${den[$i]}_accurate/accurate_${seed[$j]}_den${den[$i]}.dat -seed ${seed[$j]} -radMethod 2  -treeDist 0.3 -X 20 -Y 20 -Z 10 -appendMode
+      ./lig-radiation -generateLocations ${den[$i]} -manyTrees manytrees.txt -resultfile Resultapp/${den[$i]}_accurate/accurate_${seed[$j]}_den${den[$i]}.dat -seed ${seed[$j]} -radMethod 2  -treeDist 0.3 -X 20 -Y 20 -Z 10 -appendMode -GapRadius 0.6
   done
 done
 
--- ForestRadiation/Ranalaysis/execute_code.sh
+++ ForestRadiation/Ranalaysis/execute_code.sh
@@ -1,46 +1,60 @@
 #!/bin/bash
 # To run this script there is a need to have a folder named Resultapp that will then store all the other results in the Resultapp folder. If your choice of the name is differnt then please change it accordinly in all the places here. i.e. Find and Replace and it will do the job
 
-den=(150) # This is where the densities of the tress are given
+den=(23) # This is where the densities of the tress are given
 seed=(787237  536199  1676779  546327  235663 787231 536197 1576775 446327 135663) 
-numP=(3)
-vox=(0.1  0.2 0.3 0.4) #Please leave some space between 0.1 and 0.2 since it acts weird otherwise,
+numP=(1 3 5)
+vox=(0.1 0.2 0.3 0.4) 
 dLen=${#den[@]} #Length of the density
 sLen=${#seed[@]} # Length of the seeds list
 numPLen=${#numP[@]}
-vLen=${#vox[@]} 
+vLen=${#vox[@]}
+
+echo Densitylevels ${dLen}  NoPartsLevels ${numPLen}  Voxellevels ${vLen}  Seedlevels ${sLen} 
+
 for (( i=0; i<${dLen}; i++ ));
  do
    mkdir Resultapp/${den[$i]}
     for (( l=0; l<${numPLen}; l++ ));
     do
       mkdir Resultapp/${den[$i]}/numParts_${numP[$l]}
-   for (( j=0; j<${sLen}; j++ ));
-   do
-   echo ${den[$i]}
-   echo ${seed[$j]}
-     for (( k=0; k<${vLen};k++));
-       do
-     
-    mkdir Resultapp/${den[$i]}/numParts_${numP[$l]}/MeanStar # make changes to the path where you need to store the data here
-    mkdir Resultapp/${den[$i]}/numParts_${numP[$l]}/MeanStar/BoxDirYes  # Make changes to the path if needed here.
-     echo ${vox[$k]}
-     echo -----------------------------------------
-     ./lig-radiation -generateLocations ${den[$i]} -manyTrees manytrees.txt -resultfile Resultapp/${den[$i]}/numParts_${numP[$l]}/MeanStar/BoxDirYes/boxdirYmean_${seed[$j]}_den${den[$i]}.dat -seed ${seed[$j]} -radMethod 3  -treeDist 0.3 -X 20 -Y 20 -Z 10 -correctSTAR -boxDirEffect  -Voxboxside ${vox[$k]} -numParts ${numP[$l]}  -appendMode
- cp runfile.dat Resultapp/${den[$i]}/runfile_${seed[$j]}_${den[$i]}_BDY_${vox[$k]}.dat
+      echo Directory: Resultapp/${den[$i]}/numParts_${numP[$l]}
+
+      mkdir Resultapp/${den[$i]}/numParts_${numP[$l]}/MeanStar
+      echo Directory: Resultapp/${den[$i]}/numParts_${numP[$l]}/MeanStar
+      mkdir Resultapp/${den[$i]}/numParts_${numP[$l]}/MeanStar/BoxDirNo
+      echo Directory: Resultapp/${den[$i]}/numParts_${numP[$l]}/MeanStar/BoxDirNo
+      mkdir Resultapp/${den[$i]}/numParts_${numP[$l]}/MeanStar/BoxDirYes
+      echo Directory: Resultapp/${den[$i]}/numParts_${numP[$l]}/MeanStar/BoxDirYes
+      mkdir Resultapp/${den[$i]}/numParts_${numP[$l]}/Directional
+      echo Directory: Resultapp/${den[$i]}/numParts_${numP[$l]}/Directional
+
+      for (( j=0; j<${sLen}; j++ ));
+      do
+
+	  for (( k=0; k<${vLen};k++));
+	  do
+              echo Density ${den[$i]}  NoParts ${numP[$l]}  Voxel ${vox[$k]}   Seed ${seed[$j]}
+
+#    mkdir Resultapp/${den[$i]}/numParts_${numP[$l]}/MeanStar/BoxDirYes  # Make changes to the path if needed here.
+#     echo ${vox[$k]}
+#     echo -----------------------------------------
+     ./lig-radiation -generateLocations ${den[$i]} -manyTrees manytrees.txt -resultfile Resultapp/${den[$i]}/numParts_${numP[$l]}/MeanStar/BoxDirYes/boxdirYmean_${seed[$j]}_den${den[$i]}.dat -seed ${seed[$j]} -radMethod 3  -treeDist 0.3 -X 20 -Y 20 -Z 10 -correctSTAR -boxDirEffect  -Voxboxside ${vox[$k]} -numParts ${numP[$l]}  -appendMode -GapRadius 0.6
+     cp runfile.dat Resultapp/${den[$i]}/runfile_${seed[$j]}_${den[$i]}_BDY_${vox[$k]}.dat
 
     
 
 #*********************************************************************************************************************************
-    mkdir Resultapp/${den[$i]}/numParts_${numP[$l]}/MeanStar/BoxDirNo
-   ./lig-radiation -generateLocations ${den[$i]} -manyTrees manytrees.txt -resultfile Resultapp/${den[$i]}/numParts_${numP[$l]}/MeanStar/BoxDirNo/boxdirNmean_${seed[$j]}_den${den[$i]}.dat -seed ${seed[$j]} -radMethod 3  -treeDist 0.3 -X 20 -Y 20 -Z 10 -correctSTAR  -Voxboxside ${vox[$k]} -numParts ${numP[$l]}  -appendMode
-  cp runfile.dat Resultapp/${den[$i]}/runfile_${seed[$j]}_${den[$i]}_BDN_${vox[$k]}.dat
+
+
+   ./lig-radiation -generateLocations ${den[$i]} -manyTrees manytrees.txt -resultfile Resultapp/${den[$i]}/numParts_${numP[$l]}/MeanStar/BoxDirNo/boxdirNmean_${seed[$j]}_den${den[$i]}.dat -seed ${seed[$j]} -radMethod 3  -treeDist 0.3 -X 20 -Y 20 -Z 10 -correctSTAR  -Voxboxside ${vox[$k]} -numParts ${numP[$l]}  -appendMode -GapRadius 0.6
+#  cp runfile.dat Resultapp/${den[$i]}/runfile_${seed[$j]}_${den[$i]}_BDN_${vox[$k]}.dat
 
     
 #*********************************************************************************************************************************
-   mkdir Resultapp/${den[$i]}/numParts_${numP[$l]}/Directional # Make changes to the path if needed here.
-    ./lig-radiation -generateLocations ${den[$i]} -manyTrees manytrees.txt -resultfile Resultapp/${den[$i]}/numParts_${numP[$l]}/Directional/boxdirNdir_${seed[$j]}_den${den[$i]}.dat -seed ${seed[$j]} -radMethod 3  -treeDist 0.3 -X 20 -Y 20 -Z 10 -correctSTAR  -calculateDirectionalStar -Voxboxside ${vox[$k]} -numParts ${numP[$l]}  -appendMode
- cp runfile.dat Resultapp/${den[$i]}/runfile_${seed[$j]}_${den[$i]}_DIR_${vox[$k]}.dat
+#   mkdir Resultapp/${den[$i]}/numParts_${numP[$l]}/Directional # Make changes to the path if needed here.
+    ./lig-radiation -generateLocations ${den[$i]} -manyTrees manytrees.txt -resultfile Resultapp/${den[$i]}/numParts_${numP[$l]}/Directional/boxdirNdir_${seed[$j]}_den${den[$i]}.dat -seed ${seed[$j]} -radMethod 3  -treeDist 0.3 -X 20 -Y 20 -Z 10 -correctSTAR  -calculateDirectionalStar -Voxboxside ${vox[$k]} -numParts ${numP[$l]}  -appendMode -GapRadius 0.6
+# cp runfile.dat Resultapp/${den[$i]}/runfile_${seed[$j]}_${den[$i]}_DIR_${vox[$k]}.dat
 
       done
     done
